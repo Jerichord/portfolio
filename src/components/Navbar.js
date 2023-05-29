4c66cae608cd0a4d68955b9cad61c25cf55420da
Jerichord <54520439+Jerichord@users.noreply.github.com>
2023-05-29 12:31:16 -0700

    added a mobile section. Now just adding state logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navbar() {
+  const [navbar, setNavbar] = useState(false);
+
   return (
-    <div class="mx-auto py-5 sm:pd-20 sticky top-0 z-50 bg-white slideFromTop">
-      <nav class="max-w-7xl mx-auto sm:px-12">
+    <div class="mx-auto px-4 py-5 sm:pd-20 sticky top-0 z-50 bg-white slideFromTop ">
+      <nav class="max-w-7xl mx-auto ">
         <ul class="flex items-center font-bold">
           <div>
             <li>
-              <a href="#intro" class="hover:opacity-50 text-2xl">
+              <a href="#intro" class="hover:opacity-50 text-2xl ">
                 Jericho.Referente
               </a>
             </li>
           </div>
-          <div class="flex justify-end ml-auto gap-12 ">
+          <div class="hidden sm:flex justify-end ml-auto gap-12 ">
             <li>
               <a href="#about" class="hover:opacity-50">
                 About
@@ -39,7 +41,52 @@ export default function Navbar() {
               </a>
             </li>
           </div>
+          <div class="sm:hidden  justify-end ml-auto mr-2 ">
+            <svg
+              class="cursor-pointer"
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            >
+              <path
+                d="M24 18v1h-24v-1h24zm0-6v1h-24v-1h24zm0-6v1h-24v-1h24z"
+                fill="#1040e2"
+              />
+              <path d="M24 19h-24v-1h24v1zm0-6h-24v-1h24v1zm0-6h-24v-1h24v1z" />
+            </svg>
+          </div>
         </ul>
+        <div class="sm:hidden items-center font-bold bg-white text-center">
+          <ul class="mt-6">
+            <li class="pb-4">
+              <a href="#about" class="hover:opacity-50">
+                About
+              </a>
+            </li>
+            <li class="pb-4">
+              <a href="#works" class="hover:opacity-50">
+                Works
+              </a>
+            </li>
+            <li class="pb-4">
+              <a href="#contact" class="hover:opacity-50">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a
+                href="./resume.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="hover:opacity-50"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   );
